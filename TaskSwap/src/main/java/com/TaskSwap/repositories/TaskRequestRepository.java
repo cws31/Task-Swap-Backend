a6389a9f3f3018bb68e34610c1657b4d16a66645
sonu kumar <gautamrocky909621@gmail.com>
2025-11-13 02:58:42 +0530

    added required methods to this repo to implement that two endpoints of feetching the status of requested task or skill.

--- TaskSwap/src/main/java/com/TaskSwap/repositories/TaskRequestRepository.java
+++ TaskSwap/src/main/java/com/TaskSwap/repositories/TaskRequestRepository.java
@@ -3,7 +3,9 @@ package com.TaskSwap.repositories;
 import com.TaskSwap.entities.*;
 import com.TaskSwap.enums.RequestType;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
+import java.util.Optional;
 
 public interface TaskRequestRepository extends JpaRepository<TaskRequest, Long> {
 
@@ -18,4 +20,8 @@ public interface TaskRequestRepository extends JpaRepository<TaskRequest, Long> 
     boolean existsByRequesterAndTaskAndRequestType(User requester, Task task, RequestType type);
 
     boolean existsByRequesterAndSkillAndRequestType(User requester, Skill skill, RequestType type);
+
+    Optional<TaskRequest> findByRequesterAndTask(User requester, Task task);
+
+    Optional<TaskRequest> findByRequesterAndSkill(User requester, Skill skill);
 }
