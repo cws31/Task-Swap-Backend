e25d031f998ae050150c47d048a1c0014dcaafe5
sonu kumar <gautamrocky909621@gmail.com>
2025-11-13 02:12:57 +0530

    repo for TaskRequest entity

---
+++ TaskSwap/src/main/java/com/TaskSwap/DTOs/TaskRequestDTO.java
@@ -0,0 +1,21 @@
+package com.TaskSwap.DTOs;
+
+import com.TaskSwap.enums.RequestStatus;
+import com.TaskSwap.enums.RequestType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TaskRequestDTO {
+    private Long id;
+    private String requesterUsername;
+    private String receiverUsername;
+    private RequestType requestType;
+    private RequestStatus status;
+    private LocalDateTime createdAt;
+}
---
+++ TaskSwap/src/main/java/com/TaskSwap/repositories/TaskRequestRepository.java
@@ -0,0 +1,21 @@
+package com.TaskSwap.repositories;
+
+import com.TaskSwap.entities.*;
+import com.TaskSwap.enums.RequestType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface TaskRequestRepository extends JpaRepository<TaskRequest, Long> {
+
+    List<TaskRequest> findByRequester(User requester);
+
+    List<TaskRequest> findByReceiver(User receiver);
+
+    List<TaskRequest> findByTask_CreatedBy(User owner);
+
+    List<TaskRequest> findBySkill_User(User owner);
+
+    boolean existsByRequesterAndTaskAndRequestType(User requester, Task task, RequestType type);
+
+    boolean existsByRequesterAndSkillAndRequestType(User requester, Skill skill, RequestType type);
+}
