4ddf445e61e7630f2cb9628630db67e0e3e9ce19
sonu kumar <gautamrocky909621@gmail.com>
2025-11-13 06:39:02 +0530

    updated becuase need to display proper message in notification

--- TaskSwap/src/main/java/com/TaskSwap/DTOs/TaskRequestDTO.java
+++ TaskSwap/src/main/java/com/TaskSwap/DTOs/TaskRequestDTO.java
@@ -15,6 +15,9 @@ public class TaskRequestDTO {
     private Long id;
     private String requesterUsername;
     private String receiverUsername;
+    private String requesterFullName;
+    private String receiverFullName;
+    private String taskTitle;
     private RequestType requestType;
     private RequestStatus status;
     private LocalDateTime createdAt;
